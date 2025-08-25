c083cdd175fdad3eed6cba0be39c6755044f3640
Stella <wstellawambui@gmail.com>
2025-08-25 15:32:25 +0300

    fixed the vercel.json and inngest

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -4,66 +4,54 @@ import User from "../models/user.js";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "event-ticket-booking" });
 
-//inngest function to save user data to a database
+
+//Inngest Function to save user data to datebase
 const syncUserCreation = inngest.createFunction(
     {id: 'sync-user-from-clerk'},
-    {event: 'clerk/user.created' },
-    async ( event)=> {
+    {event: 'clerk/user.created'},
+
+    async ( {event})=>{
         const {id, first_name, last_name, email_addresses, image_url} = event.data
-        const userData ={
+        const userData = {
             _id: id,
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             image: image_url
-
         }
+      await User.create(userData)  
+    })
 
-        await User.create(userData)
-    }
-
-)
-
-//Inngest Function to delete user from database
-
+//Inngest function to delete data from database
 const syncUserDeletion = inngest.createFunction(
     {id: 'delete-user-with-clerk'},
-    {event: 'clerk/user.deleted' },
-    async ( event)=> {
+    {event: 'clerk/user.deleted'},
+
+    async ( {event})=>{
         
         const {id} = event.data
         await User.findByIdAndDelete(id)
-      
     }
-
 )
-//Inngest Function to update user from database
-
+//Inngest function to update user data in database
 const syncUserUpdation = inngest.createFunction(
     {id: 'update-user-from-clerk'},
-    {event: 'clerk/user.updated' },
-    async ( event)=> {
-       const {id, first_name, last_name, email_addresses, image_url} = event.data 
+    {event: 'clerk/user.updated'},
 
-       const userData ={
+    async ( {event})=>{
+            const {id, first_name, last_name, email_addresses, image_url} = event.data
+            
+            const userData = {
             _id: id,
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             image: image_url
-
-        } 
-
-        await User.findByIdAndUpdate(id, userData)
-      
+        }
+       await User.findByIdAndUpdate(id,userData) 
     }
-
 )
 
 
-
-
-
-// Create an empty array where we'll export future Inngest functions
-export const functions = [
+export const functions =[
     syncUserCreation,
     syncUserDeletion,
     syncUserUpdation
