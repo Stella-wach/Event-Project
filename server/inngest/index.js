4dda44c4a2ff5d7202a7362b6fa00ae5497c598a
Stella <wstellawambui@gmail.com>
2025-08-25 11:52:33 +0300

    fix: remove destructuring from delete-user-with-clerk and log payload

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,70 +1,61 @@
 import { Inngest } from "inngest";
+import User from "../models/user.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "event-ticket-booking" });
 
-//inngest function to save user data to a database
+// Inngest function to save user data to a database
 const syncUserCreation = inngest.createFunction(
-    {id: 'sync-user-from-clerk'},
-    {event: 'clerk/user.created' },
-    async ( event)=> {
-        const {id, first_name, last_name, email_addresses, image_url} = event.data
-        const userData ={
-            _id: id,
-            email: email_addresses[0].email_address,
-            name: first_name + ' ' + last_name,
-            image: image_url
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
 
-        }
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      image: image_url,
+    };
 
-        await User.create(userData)
-    }
+    await User.create(userData);
+  }
+);
 
-)
-
-//Inngest Function to delete user from database
-
-const syncUserDeletion = inngest.createFunction(
-    {id: 'delete-user-with-clerk'},
-    {event: 'clerk/user.deleted' },
-    async ( event)=> {
-        
-        const {id} = event.data
-        await User.findByIdAndDelete(id)
-      
-    }
-
-)
-//Inngest Function to update user from database
-
+// Inngest function to update user from database
 const syncUserUpdation = inngest.createFunction(
-    {id: 'update-user-from-clerk'},
-    {event: 'clerk/user.updated' },
-    async ( event)=> {
-       const {id, first_name, last_name, email_addresses, image_url} = event.data 
+  { id: "update-user-from-clerk" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
 
-       const userData ={
-            _id: id,
-            email: email_addresses[0].email_address,
-            name: first_name + ' ' + last_name,
-            image: image_url
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      image: image_url,
+    };
 
-        } 
+    await User.findByIdAndUpdate(id, userData);
+  }
+);
 
-        await User.findByIdAndUpdate(id, userData)
-      
-    }
+// 🚨 remove or comment out the destructuring version of syncUserDeletion
+// const syncUserDeletion = ...
 
-)
+// ✅ Keep only this for testing:
+export default inngest.createFunction(
+  { name: "delete-user-with-clerk" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    console.log("Clerk delete event payload:", JSON.stringify(event, null, 2));
+    return { ok: true };
+  }
+);
 
-
-
-
-
-// Create an empty array where we'll export future Inngest functions
+// Export functions
 export const functions = [
-    syncUserCreation,
-    syncUserDeletion,
-    syncUserUpdation
-
+  syncUserCreation,
+  syncUserUpdation,
+  // 🚨 leave out syncUserDeletion until we fix payload shape
 ];
