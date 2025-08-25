f6477db2128bbcae8d71b494de29c34fcd54a599
Stella <wstellawambui@gmail.com>
2025-08-25 15:32:25 +0300

    fixed the vercel.json and inngest

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try{
-        mongoose.connection.on('connected', ()=> console.log('Connected to MongoDB successfully'))
+        mongoose.connection.on('connected', ()=> console.log('Connected to MongoDB successfully'));
         await mongoose.connect(`${process.env.MONGODB_URI}/quickevents`)
     } catch (error) {
 console.log(error.message);
